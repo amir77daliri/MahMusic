d827c091eff59efb878fba66c82b26a6dd8c4f4f
Amirhossein_Daliri <amirdalir0111@gmail.com>
2022-03-24 17:11:14 +0430

    update Music Model, add Music and Account app to admin panel

--- Music/admin.py
+++ Music/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from .models import Music
 
-# Register your models here.
+
+@admin.register(Music)
+class MusicAdmin(admin.ModelAdmin):
+    list_display = ['name', 'music', 'status', 'published_at']
+    list_filter = ['created_at', 'status']
+    search_fields = ('name', 'music')
+    actions = ['make_accept', 'make_pending']
+
+
+    def make_accept(self, request, queryset):
+        changed_numbers = queryset.update(status='A')
+        if changed_numbers == 1:
+            message = "1 music status change to Accept successfully"
+        else:
+            message = "%s musics status change to Accept successfully" % changed_numbers
+        self.message_user(request, message)
+    make_accept.short_description = 'change status to Accept'
+
+    def make_pending(self, request, queryset):
+        changed_numbers = queryset.update(status='p')
+        if changed_numbers == 1:
+            message = "1 music status change to Pending successfully"
+        else:
+            message = "%s music status change to Pending successfully" % changed_numbers
+        self.message_user(request, message)
+    make_pending.short_description = 'change status to Pending'
--- Music/models.py
+++ Music/models.py
@@ -2,8 +2,6 @@ from django.db import models
 from django.utils import timezone
 from django.utils.text import slugify
 import os
-# Create your models here.
-
 
 
 def get_filename_ext(filepath):
@@ -11,9 +9,10 @@ def get_filename_ext(filepath):
     name, ext = os.path.splitext(base_name)
     return name, ext
 
-def upload_image_path(instance , filename):
+
+def upload_image_path(instance, filename):
     name, ext = get_filename_ext(filename)
-    final_name = f'{instance.name}-{instance.pk}{ext}'
+    final_name = f'{instance.name}-{ext}'
     return f'Music/images/{final_name}'
 
 
