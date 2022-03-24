e47cd3d0277f5f7a9c118299e154f7840b058b97
Amirhossein_Daliri <amirdalir0111@gmail.com>
2022-03-24 14:56:09 +0430

    set media root

--- Music/models.py
+++ Music/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+# class Music(models.Model):
+#     name = models.CharField(max_length=100)
+#     slug = models.SlugField(max_length=200, blank=True)
+#
