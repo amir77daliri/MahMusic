7e82e5cb3b0429ce499b7793eac423dc119c6b0c
Amirhossein_Daliri <amirdalir0111@gmail.com>
2022-03-24 17:58:51 +0430

    create singer model, admin panel for singer, thumbnail tag for display singer and music images in admin panel

--- Singer/admin.py
+++ Singer/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Singer
 
-# Register your models here.
+
+@admin.register(Singer)
+class SingerAdmin(admin.ModelAdmin):
+    list_display = ['name', 'thumbnail_tag']
--- Singer/models.py
+++ Singer/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.utils.text import slugify
+from django.utils.html import format_html
+from Music.models import get_filename_ext
 
-# Create your models here.
+
+def upload_image_path(instance, filename):
+    name, ext = get_filename_ext(filename)
+    final_name = f'{instance.name}-{ext}'
+    return f'Singer/images/{final_name}'
+
+
+class Singer(models.Model):
+    name = models.CharField(max_length=150)
+    slug = models.CharField(max_length=200, blank=True)
+    image = models.ImageField(upload_to=upload_image_path, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def thumbnail_tag(self):
+        return format_html("<img width=100 height=80 style='border-radius:5px;'src='{}'>".format(self.image.url))
+    thumbnail_tag.short_description = "thumbnail"
+
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Singer, self).save(*args, **kwargs)
+
