f92e0105f59fb26e1be7b5d90b8fa8a4afc0eff1
Amirhossein_Daliri <amirdalir0111@gmail.com>
2022-03-29 11:37:55 +0430

    alter User Model

--- Account/models.py
+++ Account/models.py
@@ -1,7 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-# Create your models here.
+
 
 class User(AbstractUser):
-    pass
+    email = models.EmailField(unique=True)
+    is_singer = models.BooleanField(default=False,blank=True)
+
+    def __str__(self):
+        if self.first_name and self.last_name:
+            return self.get_full_name()
+        return self.username
 
